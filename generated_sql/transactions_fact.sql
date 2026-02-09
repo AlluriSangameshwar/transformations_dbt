f777e45a5b607a1725077c9a67976141aa6deb33
ssehomedecor <ssehomedecor@gmail.com>
2026-02-09 11:12:57 +0530

    AI: auto-generate BigQuery SQL

--- generated_sql/transactions_fact.sql
+++ generated_sql/transactions_fact.sql
@@ -1,5 +1,5 @@
 ```sql
-SELECT `transaction_id`, ROUND(`amount`, 3) AS total_amount, TIMESTAMPADD(HOUR, 5.5, `event_ts`) AS transaction_time
+SELECT `transaction_id`, ROUND(`amount`, 3) AS total_amount, TIMESTAMP_ADD(UNIX_TIMESTAMP(`event_ts`), INTERVAL '7' HOUR) AS transaction_time
 FROM raw_prj.fdp_staging.transactions
-WHERE IS_NAN(`total_amount`);
+WHERE IS_NAN(`total_amount`) = 0 AND dbt_is_incremental() = TRUE;
 ```
