69f945d268088955f463b4dfddc9f23763f9ed38
AlluriSangameshwar <34869599+AlluriSangameshwar@users.noreply.github.com>
2026-02-09 10:35:58 +0530

    Delete generated_sql/transactions_fact.sql

--- generated_sql/transactions_fact.sql
+++
@@ -1,5 +0,0 @@
-```sql
-SELECT transaction_id AS id, ROUND(amount, 2) AS total_amount, TIMESTAMPADD(HOUR, 5, event_ts) AS transaction_time
-FROM `raw_prj.fdp_staging.transactions`
-WHERE amount IS NOT NULL AND amount != 'nan'
-```
