7a4c77093fad32cfd1f6de51958cffd3c68c10bd
AlluriSangameshwar <34869599+AlluriSangameshwar@users.noreply.github.com>
2026-02-09 10:48:19 +0530

    Update transactions_fact.sql

--- generated_sql/transactions_fact.sql
+++ generated_sql/transactions_fact.sql
@@ -1,5 +1,1 @@
-```sql
-SELECT `transaction_id`, ROUND(`amount`, 3) AS total_amount, TIMESTAMPADD(HOUR, 5.5, UNIX_TIMESTAMP(`event_ts`)) AS transaction_time
-FROM raw_prj.fdp_staging.transactions t
-WHERE IS_NAN(`amount`) = FALSE AND dbt_is_incremental() = TRUE;
-```
+
